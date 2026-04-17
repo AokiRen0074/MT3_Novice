7961c1d3d532b5bbca8b149bfb317546f016eddd
AokiRen <k025g0074@g.neec.ac.jp>
2026-04-17 11:52:11 +0900

    三次元での回転の実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,87 @@
 #include <Novice.h>
+#include <cmath>
 
-const char kWindowTitle[] = "LC1B_01_アオキレン_タイトル";
+const char kWindowTitle[] = "LC1B_01_アオキレン_MT3_0-4確認課題";
+
+//4x4の行列
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+// 三次元ベクトル
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+// 4x4行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result.m[i][j] = 0.0f;
+			for (int k = 0; k < 4; ++k) {
+				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+	return result;
+}
+
+// 4x4　X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result = {
+		{
+			{1.0f, 0.0f, 0.0f, 0.0f},
+			{0.0f, std::cos(radian), std::sin(radian), 0.0f},
+			{0.0f, -std::sin(radian), std::cos(radian), 0.0f},
+			{0.0f, 0.0f, 0.0f, 1.0f},
+		}
+	};
+	return result;
+}
+
+// 4x4　Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result = {
+		{
+			{std::cos(radian), 0.0f, -std::sin(radian), 0.0f},
+			{0.0f, 1.0f, 0.0f, 0.0f},
+			{std::sin(radian), 0.0f, std::cos(radian), 0.0f},
+			{0.0f, 0.0f, 0.0f, 1.0f},
+		}
+	};
+	return result;
+}
+
+// 4x4　Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result = {
+		{
+			{std::cos(radian), std::sin(radian), 0.0f, 0.0f},
+			{-std::sin(radian), std::cos(radian), 0.0f, 0.0f},
+			{0.0f, 0.0f, 1.0f, 0.0f},
+			{0.0f, 0.0f, 0.0f, 1.0f},
+		}
+	};
+	return result;
+}
+
+// 4x4行列の数値表示
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+
+	Novice::ScreenPrintf(x, y, "%s", label);
+
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%.2f", matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -9,8 +90,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	/*----------------------
+	初期化
+	------------------------*/
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +111,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +123,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix, "rotateYMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix, "rotateZMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix, "rotateXYZMatrix");
 
 		///
 		/// ↑描画処理ここまで
