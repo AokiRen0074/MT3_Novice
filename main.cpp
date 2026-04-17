96b9a7da3d73406fe1c634f0e96b1b03540f0c3b
AokiRen <k025g0074@g.neec.ac.jp>
2026-04-17 12:04:13 +0900

    三次元アフィン変換行列の実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,129 @@
 #include <Novice.h>
+#include <cmath>
 
-const char kWindowTitle[] = "LC1B_01_アオキレン_タイトル";
+const char kWindowTitle[] = "LC1B_01_MT3_0-5確認課題";
+
+//4x4の行列
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+// 三次元ベクトル
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+// 4x4行列の平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result = {};
+	result.m[0][0] = 1.0f;
+	result.m[1][1] = 1.0f;
+	result.m[2][2] = 1.0f;
+	result.m[3][3] = 1.0f;
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	return result;
+}
+
+// 4x4行列の拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result = {};
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1.0f;
+	return result;
+}
+
+// 4x4行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result.m[i][j] = 0.0f;
+			for (int k = 0; k < 4; ++k) {
+				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+	return result;
+}
+
+// 4x4　X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result = {
+		{
+			{1.0f, 0.0f, 0.0f, 0.0f},
+			{0.0f, std::cos(radian), std::sin(radian), 0.0f},
+			{0.0f, -std::sin(radian), std::cos(radian), 0.0f},
+			{0.0f, 0.0f, 0.0f, 1.0f},
+		}
+	};
+	return result;
+}
+
+// 4x4　Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result = {
+		{
+			{std::cos(radian), 0.0f, -std::sin(radian), 0.0f},
+			{0.0f, 1.0f, 0.0f, 0.0f},
+			{std::sin(radian), 0.0f, std::cos(radian), 0.0f},
+			{0.0f, 0.0f, 0.0f, 1.0f},
+		}
+	};
+	return result;
+}
+
+// 4x4　Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result = {
+		{
+			{std::cos(radian), std::sin(radian), 0.0f, 0.0f},
+			{-std::sin(radian), std::cos(radian), 0.0f, 0.0f},
+			{0.0f, 0.0f, 1.0f, 0.0f},
+			{0.0f, 0.0f, 0.0f, 1.0f},
+		}
+	};
+	return result;
+}
+
+
+
+// 三次元アフィン変換行列 
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Matrix4x4 rotXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+
+
+	Matrix4x4 rotateMatrix = Multiply(rotXMatrix, Multiply(rotYMatrix, rotZMatrix));
+
+	Matrix4x4 result = Multiply(scaleMatrix, Multiply(rotateMatrix, translateMatrix));
+
+	return result;
+}
+
+// 4x4行列の数値表示
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+
+	Novice::ScreenPrintf(x, y, "%s", label);
+
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%.2f", matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -11,6 +134,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	/*-------------------------
+	初期化
+	----------------------------*/
+	Vector3 scale{ 1.2f, 0.79f, -2.1f };
+	Vector3 rotate{ 0.4f, 1.43f, -0.8f };
+	Vector3 translate{ 2.7f, -4.15f, 1.57f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -24,7 +154,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓更新処理ここから
 		///
-
+		Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 		///
 		/// ↑更新処理ここまで
 		///
@@ -33,6 +163,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		/// ↓描画処理ここから
 		///
 
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
