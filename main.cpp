7576b7dae4744624bdb6fd8303a47326f99b2699
AokiRen <k025g0074@g.neec.ac.jp>
2026-06-11 22:11:33 +0900

    指摘事項修正

--- main.cpp
+++ main.cpp
@@ -525,19 +525,27 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		ImGui::End();
 
 		// 衝突判定を行う
-		aabb1.min.x = (std::min)(aabb1.min.x, aabb1.max.x);
-		aabb1.max.x = (std::max)(aabb1.min.x, aabb1.max.x);
-		aabb1.min.y = (std::min)(aabb1.min.y, aabb1.max.y);
-		aabb1.max.y = (std::max)(aabb1.min.y, aabb1.max.y);
-		aabb1.min.z = (std::min)(aabb1.min.z, aabb1.max.z);
-		aabb1.max.z = (std::max)(aabb1.min.z, aabb1.max.z);
+		
+		Vector3 tempMin = aabb1.min;
+		Vector3 tempMax = aabb1.max;
 
-		aabb2.min.x = (std::min)(aabb2.min.x, aabb2.max.x);
-		aabb2.max.x = (std::max)(aabb2.min.x, aabb2.max.x);
-		aabb2.min.y = (std::min)(aabb2.min.y, aabb2.max.y);
-		aabb2.max.y = (std::max)(aabb2.min.y, aabb2.max.y);
-		aabb2.min.z = (std::min)(aabb2.min.z, aabb2.max.z);
-		aabb2.max.z = (std::max)(aabb2.min.z, aabb2.max.z);
+		aabb1.min.x = (std::min)(tempMin.x, tempMax.x);
+		aabb1.max.x = (std::max)(tempMin.x, tempMax.x);
+		aabb1.min.y = (std::min)(tempMin.y, tempMax.y);
+		aabb1.max.y = (std::max)(tempMin.y, tempMax.y);
+		aabb1.min.z = (std::min)(tempMin.z, tempMax.z);
+		aabb1.max.z = (std::max)(tempMin.z, tempMax.z);
+
+		
+		Vector3 tempMin2 = aabb2.min;
+		Vector3 tempMax2 = aabb2.max;
+		aabb2.min.x = (std::min)(tempMin2.x, tempMax2.x);
+		aabb2.max.x = (std::max)(tempMin2.x, tempMax2.x);
+		aabb2.min.y = (std::min)(tempMin2.y, tempMax2.y);
+		aabb2.max.y = (std::max)(tempMin2.y, tempMax2.y);
+		aabb2.min.z = (std::min)(tempMin2.z, tempMax2.z);
+		aabb2.max.z = (std::max)(tempMin2.z, tempMax2.z);
+		
 
 		bool isColliding = IsCollision(aabb1, aabb2);
 		uint32_t lineColor = isColliding ? 0xFF0000FF : 0xF4FBFEFF;
